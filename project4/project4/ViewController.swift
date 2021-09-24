deb56892f629ff094158392972872071f39a4d79
ucwealth <ucheokeke16@gmail.com>
2021-09-24 13:42:16 +0100

    create web view

---
+++ project4/project4/ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  project4
+//
+//  Created by Decagon on 24/09/2021.
+//
+
+import UIKit
+import WebKit
+
+class ViewController: UIViewController, WKNavigationDelegate {
+    var webView: WKWebView!
+    
+    override func loadView() {
+        webView = WKWebView()
+        webView.navigationDelegate = self
+        view = webView
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let url = URL(string: "https://www.hackingwithswift.com")!
+        webView.load(URLRequest(url: url))
+        webView.allowsBackForwardNavigationGestures = true 
+    }
+
+
+}
+
