2dbdd0771e2dd4f0110a9b26d26120e6c1dd92df
ucwealth <ucheokeke16@gmail.com>
2021-10-20 15:01:28 +0100

    Create LCO SwiftUI App

---
+++ LcoSwiftuiApp/LcoSwiftuiApp/LcoSwiftuiAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  LcoSwiftuiAppApp.swift
+//  LcoSwiftuiApp
+//
+//  Created by Decagon on 20/10/2021.
+//
+
+import SwiftUI
+
+@main
+struct LcoSwiftuiAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
