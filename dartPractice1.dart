eacb877a4474242241bf5e78608c7f853a69ba04
ucwealth <ucheokeke16@gmail.com>
2021-10-21 23:46:32 +0100

    work with lists

--- dartPractice1.dart
+++ dartPractice1.dart
@@ -10,16 +10,31 @@ void main() {
   // print(int.parse(firstNum!) + int.parse(secondNum!));
 
   // Mad Libs Game
-  print("Enter a color: ");
-  String? color = stdin.readLineSync();
 
-  print("Enter a plural noun: ");
-  String? noun = stdin.readLineSync();
+  // print("Enter a color: ");
+  // String? color = stdin.readLineSync();
 
-  print("Enter a celebrity name: ");
-  String? celebrity = stdin.readLineSync();
-  print(" ");
-  print("Roses are ${color}");
-  print("${noun} are blue");
-  print("I love ${celebrity}");
+  // print("Enter a plural noun: ");
+  // String? noun = stdin.readLineSync();
+
+  // print("Enter a celebrity name: ");
+  // String? celebrity = stdin.readLineSync();
+  // print(" ");
+  // print("Roses are ${color}");
+  // print("${noun} are blue");
+  // print("I love ${celebrity}");
+
+  // Arrays and Lists
+  List<int> favNumbers = [3, 6, 9, 12, 15];
+  favNumbers.add(18);
+  favNumbers.remove(6);
+  favNumbers.indexOf(6);
+  print(favNumbers);
+
+  print(addNum(5, 2));
+}
+
+// Functions
+double addNum(double num1, double num2) {
+  return num1 + num2;
 }
