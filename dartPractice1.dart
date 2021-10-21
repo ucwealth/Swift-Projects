d00655026bdd9a8b979f7e2e9910d9cb075f7052
ucwealth <ucheokeke16@gmail.com>
2021-10-21 23:16:33 +0100

    create mad libs game

--- dartPractice1.dart
+++ dartPractice1.dart
@@ -1,9 +1,25 @@
 import 'dart:io';
 
 void main() {
-  print("Type first number: ");
-  String? firstNum = stdin.readLineSync();
-  print("Type second number: ");
-  String? secondNum = stdin.readLineSync();
-  print(int.parse(firstNum!) + int.parse(secondNum!));
+  // Basic Calculator
+
+  // print("Type first number: ");
+  // String? firstNum = stdin.readLineSync();
+  // print("Type second number: ");
+  // String? secondNum = stdin.readLineSync();
+  // print(int.parse(firstNum!) + int.parse(secondNum!));
+
+  // Mad Libs Game
+  print("Enter a color: ");
+  String? color = stdin.readLineSync();
+
+  print("Enter a plural noun: ");
+  String? noun = stdin.readLineSync();
+
+  print("Enter a celebrity name: ");
+  String? celebrity = stdin.readLineSync();
+  print(" ");
+  print("Roses are ${color}");
+  print("${noun} are blue");
+  print("I love ${celebrity}");
 }
