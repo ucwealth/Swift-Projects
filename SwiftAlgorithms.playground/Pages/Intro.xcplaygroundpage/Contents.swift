0ccf3bcf64e928bb63bb0eef802c0e15bd489b43
ucwealth <ucheokeke16@gmail.com>
2025-01-26 21:11:29 +0000

    edit intro page

--- SwiftAlgorithms.playground/Pages/Intro.xcplaygroundpage/Contents.swift
+++ SwiftAlgorithms.playground/Pages/Intro.xcplaygroundpage/Contents.swift
@@ -1,7 +1,13 @@
-//: # Introduction
-
-import Foundation
-
-var greeting = "Hello, playground"
+/*:
+ # Introduction
+ This playground pages will contain my solutions to swift algorithm problems from **codewars** and **hackerrank**, as well as my *well detailed* breakdown of some swift language and iOS concepts.
+ 
+ ### Table of Contents
+ 1. Basic Algo
+ 2. Hashable
+ 3. Equatable
+ 4.
+ 
+ */
 
 //: [Next](@next)
